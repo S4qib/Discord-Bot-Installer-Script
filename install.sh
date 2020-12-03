5682a91f7a92b72530ee4cdc7e9466c3f3400e75
S4qib <73705449+S4qib@users.noreply.github.com>
2020-12-03 22:51:50 +0000

    Update install.sh
    Added 2 more bots to install script (EvoBot and AtlantaBot)

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 echo "------------------------"
 echo "|Discord bot installler!"
 echo "|Copyright (C) Feroxus Inc, all rights reserved."
-echo "|Made with <3 by Saqib, licensed under Apache 2.0 License"
+echo "|Made with <3 by Saqib, licensed under MIT"
 echo "------------------------"
 
 echo ""
@@ -20,16 +20,41 @@ fi
 
 DoritoBot() {
     git clone https://github.com/Aaronidk/Dorito-bot-discord.js.git
+    echo "---------------"
     echo "Succesfuly installed! Head over to directory Dortio-bot-discord.js to config the bot!"
+    echo "---------------"
+    echo "To start bot please do commands cd Dorito-bot-discord.js then node index.js after bot is configured!"
+    echo "---------------"
 }
 
 RedBot() {
     git clone https://github.com/Cog-Creators/Red-DiscordBot.git
+    echo "---------------"
     echo "Succesfuly installed! Head over to directory Red-DiscordBot to config the bot!"
+    echo "---------------"
+    echo "To start bot please do commands cd Red-DiscordBot then py launcher.py(Windows) or python3 launcher.py(Linux/MacOS) after bot is configured!"
+    echo "---------------"
 }
-
+AtlantaBot() {
+    git clone https://github.com/Androz2091/AtlantaBot.git
+    echo "---------------"
+    echo "Succesfuly installed! Head over to directory AtlantaBot to config the bot!"
+    echo "---------------"
+    echo "To start bot please do commands cd AtlantaBot then node atlanta.js after bot is configured!"
+    echo "---------------"    
+}
+EvoBot() {
+    git clone https://github.com/eritislami/evobot.git
+    echo "---------------"
+    echo "Succesfuly installed! Head over to directory EvoBot to config the bot!"
+    echo "---------------"
+    echo "To start bot please do commands cd evobot then node index.js after bot is configured!"
+    echo "---------------"
+}
   echo "[1] Install DoritoBot"
   echo "[2] Install RedBot"
+  echo "[3] Install AtlantaBot"
+  echo "[4] Install EvoBot"
 
   read -r action
 
@@ -38,6 +63,10 @@ RedBot() {
           DoritoBot ;;
       2 )
           RedBot ;;
+      3) 
+          AtlantaBot ;;
+      4)
+          EvoBot ;;
                 * )
           error "Invalid option"
 esac
